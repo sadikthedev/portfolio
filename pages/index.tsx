e7a5cb39b6cedc8c894d750dda33144e1bb89af6
sadikthedev <sadikthedev@gmail.com>
2024-04-03 22:34:30 +0530

    leftside icons & rightside mail fixed

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,6 @@
+import LeftSide from '@/components/LeftSide'
 import Navbar from '@/components/Navbar'
+import RightSide from '@/components/RightSide'
 import Head from 'next/head'
 
 export default function Home() {
@@ -13,6 +15,15 @@ export default function Home() {
       <main className="w-full h-screen font-bodyFont bg-bodyColor text-textLight
       overflow-x-hidden overflow-y-scroll">
         <Navbar />
+        <div className="w-full h-[88vh] xl:flex items-center gap-20 justify-between">
+          <div className="hidden xl:inline-flex w-32 h-full fixed left-0 bottom-0">
+            <LeftSide/>
+          </div>
+          <div>Middle</div>
+          <div className="hidden xl:inline-flex w-32 h-full fixed right-0 bottom-0">
+            <RightSide/>
+          </div>
+        </div>
       </main>
     </>
   )
