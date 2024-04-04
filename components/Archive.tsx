306b0c288191804645b83158f7bc7e9337426aaa
sadikthedev <sadikthedev@gmail.com>
2024-04-04 12:47:07 +0530

    archive & archive cards listed

--- components/Archive.tsx
+++ components/Archive.tsx
@@ -1,7 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ArchiveCard from './ArchiveCard'
+import {motion} from 'framer-motion'
 
 const Archive = () => {
+  const [showMore, setShowMore] = useState(false);  
   return (
     <div className='max-w-contentContainer mx-auto px-4 py-24'>
       <div className='w-full flex flex-col items-center'>
@@ -12,8 +14,156 @@ const Archive = () => {
           view the archive
         </p>
       </div>
-      <div>
-        <ArchiveCard/>
+      <div className='grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6 mt-10
+      lgl:px-10'>
+        <ArchiveCard
+          title="Educare English Learning Academy"
+          des="Lorem ipsum dolor sit amet consectetur adipisicing elit.
+           Aliquid fuga tempora consectetur? Nihil consequuntur sapiente 
+           quae recusandae commodi expedita doloremque!"
+           listItem={["Reactjs", "Tailwind CSS", "getform.io"]}
+           link="https://www.educareenglishlearningacademy.com/"
+        />
+        <ArchiveCard
+          title="Educare English Learning Academy"
+          des="Lorem ipsum dolor sit amet consectetur adipisicing elit.
+           Aliquid fuga tempora consectetur? Nihil consequuntur sapiente 
+           quae recusandae commodi expedita doloremque!"
+           listItem={["Reactjs", "Tailwind CSS", "getform.io"]}
+           link="https://www.educareenglishlearningacademy.com/"
+        />
+        <ArchiveCard
+          title="Educare English Learning Academy"
+          des="Lorem ipsum dolor sit amet consectetur adipisicing elit.
+           Aliquid fuga tempora consectetur? Nihil consequuntur sapiente 
+           quae recusandae commodi expedita doloremque!"
+           listItem={["Reactjs", "Tailwind CSS", "getform.io"]}
+           link="https://www.educareenglishlearningacademy.com/"
+        />
+        <ArchiveCard
+          title="Educare English Learning Academy"
+          des="Lorem ipsum dolor sit amet consectetur adipisicing elit.
+           Aliquid fuga tempora consectetur? Nihil consequuntur sapiente 
+           quae recusandae commodi expedita doloremque!"
+           listItem={["Reactjs", "Tailwind CSS", "getform.io"]}
+           link="https://www.educareenglishlearningacademy.com/"
+        />
+        <ArchiveCard
+          title="Educare English Learning Academy"
+          des="Lorem ipsum dolor sit amet consectetur adipisicing elit.
+           Aliquid fuga tempora consectetur? Nihil consequuntur sapiente 
+           quae recusandae commodi expedita doloremque!"
+           listItem={["Reactjs", "Tailwind CSS", "getform.io"]}
+           link="https://www.educareenglishlearningacademy.com/"
+        />
+
+        {
+          showMore && (
+            <>
+              <motion.div
+                initial={{opacity: 0}}
+                whileInView={{opacity:1}}
+                transition={{delay: 0.1}}
+              >
+                <ArchiveCard
+                  title="Educare English Learning Academy"
+                  des="Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Aliquid fuga tempora consectetur? Nihil consequuntur sapiente 
+                  quae recusandae commodi expedita doloremque!"
+                  listItem={["Reactjs", "Tailwind CSS", "getform.io"]}
+                  link="https://www.educareenglishlearningacademy.com/"
+                />
+              </motion.div>
+              <motion.div
+                initial={{opacity: 0}}
+                whileInView={{opacity:1}}
+                transition={{delay: 0.1}}
+              >
+                <ArchiveCard
+                  title="Educare English Learning Academy"
+                  des="Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Aliquid fuga tempora consectetur? Nihil consequuntur sapiente 
+                  quae recusandae commodi expedita doloremque!"
+                  listItem={["Reactjs", "Tailwind CSS", "getform.io"]}
+                  link="https://www.educareenglishlearningacademy.com/"
+                />
+              </motion.div>
+              <motion.div
+                initial={{opacity: 0}}
+                whileInView={{opacity:1}}
+                transition={{delay: 0.1}}
+              >
+                <ArchiveCard
+                  title="Educare English Learning Academy"
+                  des="Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Aliquid fuga tempora consectetur? Nihil consequuntur sapiente 
+                  quae recusandae commodi expedita doloremque!"
+                  listItem={["Reactjs", "Tailwind CSS", "getform.io"]}
+                  link="https://www.educareenglishlearningacademy.com/"
+                />
+              </motion.div>
+              <motion.div
+                initial={{opacity: 0}}
+                whileInView={{opacity:1}}
+                transition={{delay: 0.1}}
+              >
+                <ArchiveCard
+                  title="Educare English Learning Academy"
+                  des="Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Aliquid fuga tempora consectetur? Nihil consequuntur sapiente 
+                  quae recusandae commodi expedita doloremque!"
+                  listItem={["Reactjs", "Tailwind CSS", "getform.io"]}
+                  link="https://www.educareenglishlearningacademy.com/"
+                />
+              </motion.div>
+              <motion.div
+                initial={{opacity: 0}}
+                whileInView={{opacity:1}}
+                transition={{delay: 0.1}}
+              >
+                <ArchiveCard
+                  title="Educare English Learning Academy"
+                  des="Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Aliquid fuga tempora consectetur? Nihil consequuntur sapiente 
+                  quae recusandae commodi expedita doloremque!"
+                  listItem={["Reactjs", "Tailwind CSS", "getform.io"]}
+                  link="https://www.educareenglishlearningacademy.com/"
+                />
+              </motion.div>
+              <motion.div
+                initial={{opacity: 0}}
+                whileInView={{opacity:1}}
+                transition={{delay: 0.1}}
+              >
+                <ArchiveCard
+                  title="Educare English Learning Academy"
+                  des="Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Aliquid fuga tempora consectetur? Nihil consequuntur sapiente 
+                  quae recusandae commodi expedita doloremque!"
+                  listItem={["Reactjs", "Tailwind CSS", "getform.io"]}
+                  link="https://www.educareenglishlearningacademy.com/"
+                />
+              </motion.div>
+            </>
+          )
+        }
+      </div>
+      <div className='mt-12 flex items-center justify-center'>
+        {
+          showMore ? <button
+          onClick={() => setShowMore(false)}
+          className='w-36 h-12 rounded-md text-textGreen text-[13px] border
+          border-textGreen hover:bg-hoverColor duration-300'
+        >
+          Show less
+          </button> : <button
+          onClick={() => setShowMore(true)}
+          className='w-36 h-12 rounded-md text-textGreen text-[13px] border
+          border-textGreen hover:bg-hoverColor duration-300'
+        >
+          Show more
+        </button>
+        }
       </div>  
     </div>
   )
